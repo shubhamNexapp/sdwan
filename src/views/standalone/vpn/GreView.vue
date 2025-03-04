df3b82e7fa59a7c43d220c48c9c98ed090fa0931
shubhamNexapp <shubham.s@nexapp.co.in>
2025-03-04 13:30:55 +0530

    Gre CRUD done

--- src/views/standalone/vpn/GreView.vue
+++ src/views/standalone/vpn/GreView.vue
@@ -18,12 +18,11 @@ import { useUciPendingChangesStore } from '@/stores/standalone/uciPendingChanges
 import { onMounted, onUnmounted, ref } from 'vue'
 import { ubusCall } from '@/lib/standalone/ubus'
 import TunnelTable from '@/components/standalone/ipsec_tunnel/TunnelTable.vue'
-import DeleteTunnelModal from '@/components/standalone/ipsec_tunnel/DeleteTunnelModal.vue'
+import DeleteTunnelModal from '@/components/standalone/gre/GreDelete.vue'
 import GreDrawer from '@/components/standalone/gre/GreDrawer.vue'
 import axios from 'axios'
 import { getSDControllerApiEndpoint } from '@/lib/config'
-
-
+import GreEdit from '@/components/standalone/gre/GreEdit.vue'
 
 export type IpsecTunnel = {
   id: string
@@ -32,6 +31,18 @@ export type IpsecTunnel = {
   remote: string[]
   enabled: '0' | '1'
   connected: boolean
+  tunnelName: string
+}
+
+export type IpsecTunnel2 = {
+  id: string
+  name: string
+  local: string[]
+  remote: string[]
+  enabled: '0' | '1'
+  connected: boolean
+  tunnelName: string
+  itemToEdit: object
 }
 
 const { t } = useI18n()
@@ -41,8 +52,12 @@ const RELOAD_INTERVAL = 10000
 const loading = ref(true)
 const tunnels = ref([])
 const selectedTunnel = ref<IpsecTunnel | null>(null)
+const selectedTunnel2 = ref<IpsecTunnel2 | null>(null)
 const showCreateEditDrawer = ref(false)
+const showCreateEditDrawer2 = ref(false)
 const showDeleteModal = ref(false)
+const showEditModal = ref(false)
+const showDeleteModal2 = ref(false)
 const fetchTunnelsIntervalId = ref(0)
 
 const error = ref({
@@ -74,10 +89,20 @@ function openCreateEditDrawer(itemToEdit: IpsecTunnel | null) {
   showCreateEditDrawer.value = true
 }
 
+const selectedTunnelName = ref<string | null>(null);
+const iteEdit = ref<object | null>(null);
 
-function openDeleteModal(itemToDelete: IpsecTunnel) {
-  selectedTunnel.value = itemToDelete
+function openDeleteModal(tunnelName: string) {
+  selectedTunnelName.value = tunnelName
   showDeleteModal.value = true
+}
+
+const selectedTunnels = ref();
+const showEditModals = ref(false);
+
+function openEditModal(tunnel: object) {
+  selectedTunnels.value = tunnel;
+  showEditModals.value = true;
 }
 
 function closeModalsAndDrawers() {
@@ -129,30 +154,26 @@ onUnmounted(() => {
   }
 })
 
-const apiResponse = ref()
+let apiResponse = ref()
 const getLists = async () => {
 
-try {
+  try {
 
-  loading.value = true;
-  const response = await axios.post(`${getSDControllerApiEndpoint()}/gre`, {
-    method: 'get-config',
-    payload: {}
-  });
-  
-  if(response.data.code === 200){
+    loading.value = true;
+    const response = await axios.post(`${getSDControllerApiEndpoint()}/gre`, {
+      method: 'get-config',
+      payload: {}
+    });
+
+    if (response.data.code === 200) {
+      loading.value = false;
+      apiResponse.value = response.data.data // Store API response
+    }
+  } catch (err) {
+    loading.value = false;
+  }
   loading.value = false;
-//   apiResponse.value = [response.data.data] // Store API response
-  apiResponse.value = [] // Store API response
-}
-} catch (err) {
-  loading.value = false;
-  console.error("Error:====", err);
-} 
-loading.value = false;
 };
-
-console.log("apiResponse========",apiResponse)
 
 </script>
 
@@ -164,7 +185,6 @@ console.log("apiResponse========",apiResponse)
         {{ t('common.data_updated_every_seconds', { seconds: 10 }) }}
       </div>
     </div>
-
     <div class="space-y-6">
       <div class="flex flex-col items-start justify-between gap-6 lg:flex-row">
         <p class="max-w-2xl text-sm font-normal text-gray-500 dark:text-gray-400">
@@ -172,12 +192,8 @@ console.log("apiResponse========",apiResponse)
         </p>
       </div>
 
-      <NeInlineNotification
-        kind="error"
-        :title="error.notificationTitle"
-        :description="error.notificationDescription"
-        v-if="error.notificationTitle"
-      >
+      <NeInlineNotification kind="error" :title="error.notificationTitle" :description="error.notificationDescription"
+        v-if="error.notificationTitle">
         <template #details v-if="error.notificationDetails">
           {{ error.notificationDetails }}
         </template>
@@ -186,84 +202,79 @@ console.log("apiResponse========",apiResponse)
       <NeSkeleton v-if="loading" :lines="8" size="lg" />
 
       <template v-else>
-        <!-- Show table if apiresponse has values -->
-        <NeTable v-if="apiResponse.length > 0" cardBreakpoint="md" class="mt-2">
-      <NeTableHead>
-        <NeTableHeadCell>Local Public Key</NeTableHeadCell>
-        <NeTableHeadCell>Local Network</NeTableHeadCell>
-        <NeTableHeadCell>Listen Port</NeTableHeadCell>
-        <NeTableHeadCell>Server IP</NeTableHeadCell>
-        <NeTableHeadCell>Server Port</NeTableHeadCell>
-        <NeTableHeadCell>Peer Public Key</NeTableHeadCell>
-        <NeTableHeadCell>Allowed IPs</NeTableHeadCell>
-        <!-- <NeTableHeadCell></NeTableHeadCell> -->
-      </NeTableHead>
-      <NeTableBody>
-  <NeTableRow v-for="(item, index) in apiResponse" :key="index">
-    <NeTableCell :data-label="t('standalone.real_time_monitor.interface')">
-      {{ item.local_public_key }}
-    </NeTableCell>
-    <NeTableCell :data-label="t('standalone.real_time_monitor.interface')">
-      {{ item.local_network }}
-    </NeTableCell>
-    <NeTableCell :data-label="t('standalone.real_time_monitor.interface')">
-      {{ item.listen_port }}
-    </NeTableCell>
-    <NeTableCell :data-label="t('standalone.real_time_monitor.interface')">
-      {{ item.server_ip }}
-    </NeTableCell>
-    <NeTableCell :data-label="t('standalone.real_time_monitor.interface')">
-      {{ item.server_port }}
-    </NeTableCell>
-    <NeTableCell :data-label="t('standalone.real_time_monitor.interface')">
-      {{ item.peer_public_key }}
-    </NeTableCell>
-    <NeTableCell :data-label="t('standalone.real_time_monitor.interface')">
-      {{ item.allowed_ips }}
-    </NeTableCell>
-    <!--  <NeTableCell >
-      <NeButton
-          kind="primary"
-          size="lg"
-          @click.prevent="openCreateEditDrawer(item)"
-        >
-          Edit
-        </NeButton>
-    </NeTableCell> -->
-  </NeTableRow>
-</NeTableBody>
-    </NeTable>
         <!-- Show "Add WireGuard Tunnel" button if dummyData is empty -->
-        <NeEmptyState
-          v-else
-          :title="t('standalone.wire_guard.no_wire_guard_found')"
-          :icon="['fas', 'globe']"
-        >
-          <NeButton kind="primary" @click="openCreateEditDrawer(null)">
-            <template #prefix>
-              <font-awesome-icon :icon="['fas', 'circle-plus']" class="h-4 w-4" aria-hidden="true" />
-            </template>
-            {{ t('standalone.wire_guard.add_wire_guard_tunnel') }}
-          </NeButton>
-        </NeEmptyState>
+
+        <NeButton kind="primary" @click="openCreateEditDrawer(null)">
+          <template #prefix>
+            <font-awesome-icon :icon="['fas', 'circle-plus']" class="h-4 w-4" aria-hidden="true" />
+          </template>
+          {{ t('standalone.wire_guard.add_wire_guard_tunnel') }}
+        </NeButton>
+        <!-- Show table if apiresponse has values -->
+        <NeTable cardBreakpoint="md" class="mt-2">
+          <NeTableHead>
+            <NeTableHeadCell>#</NeTableHeadCell>
+            <NeTableHeadCell>Tunnel Name</NeTableHeadCell>
+            <NeTableHeadCell>Service</NeTableHeadCell>
+            <NeTableHeadCell>Local Virtual IP</NeTableHeadCell>
+            <NeTableHeadCell>Peer Virtual IP</NeTableHeadCell>
+            <NeTableHeadCell>Local Extern IP</NeTableHeadCell>
+            <NeTableHeadCell>Peer Extern IP</NeTableHeadCell>
+            <NeTableHeadCell>Netmask</NeTableHeadCell>
+            <NeTableHeadCell>Interface Name</NeTableHeadCell>
+            <NeTableHeadCell>Key</NeTableHeadCell>
+            <NeTableHeadCell>MTU</NeTableHeadCell>
+            <NeTableHeadCell></NeTableHeadCell>
+
+          </NeTableHead>
+          <NeTableBody>
+            <NeTableRow v-for="(item, index) in apiResponse" :key="index">
+              <NeTableCell>{{ index + 1 }}</NeTableCell>
+              <NeTableCell>{{ item.tunnel_name }}</NeTableCell>
+              <NeTableCell>{{ item.config.service }}</NeTableCell>
+              <NeTableCell>{{ item.config.Local_virtual_ip }}</NeTableCell>
+              <NeTableCell>{{ item.config.peer_virtual_ip }}</NeTableCell>
+              <NeTableCell>{{ item.config.Local_extern_ip }}</NeTableCell>
+              <NeTableCell>{{ item.config.peer_extern_ip }}</NeTableCell>
+              <NeTableCell>{{ item.config.netmask }}</NeTableCell>
+              <NeTableCell>{{ item.config.interface_name }}</NeTableCell>
+              <NeTableCell>{{ item.config.key }}</NeTableCell>
+              <NeTableCell>{{ item.config.mtu }}</NeTableCell>
+              <NeTableCell :data-label="t('common.actions')">
+                <div class="-ml-2.5 flex gap-2 xl:ml-0 xl:justify-end">
+                  <NeButton kind="tertiary" size="lg" :disabled="item.readonly" @click="openEditModal(item)">
+                    <template #prefix>
+                      <font-awesome-icon :icon="['fas', 'pen-to-square']" class="h-4 w-4" aria-hidden="true" />
+                    </template>
+                    {{ t('common.edit') }}
+                  </NeButton>
+                  <NeButton kind="tertiary" size="lg" :disabled="item.readonly"
+                    @click="openDeleteModal(item.tunnel_name)">
+                    <template #prefix>
+                      <font-awesome-icon :icon="['fas', 'trash']" class="h-4 w-4" aria-hidden="true" />
+                    </template>
+                    {{ t('common.delete') }}
+                  </NeButton>
+                </div>
+              </NeTableCell>
+              <!-- <NeTableCell>
+                <NeButton kind="primary" @click="openEditModal(item)">
+                  edit</NeButton>
+                <NeButton kind="primary" @click="openDeleteModal(item.tunnel_name)">
+                  Delete
+                </NeButton>
+              </NeTableCell> -->
+            </NeTableRow>
+          </NeTableBody>
+        </NeTable>
       </template>
     </div>
   </div>
 
-  <DeleteTunnelModal
-    :visible="showDeleteModal"
-    :item-to-delete="selectedTunnel"
-    @close="closeModalsAndDrawers"
-    @tunnel-deleted="reloadTunnels"
-  />
-  <GreDrawer
-    :item-to-edit="selectedTunnel"
-    @close="closeModalsAndDrawers"
-    :rule-type="'forward'"  
-    :known-tags="[]" 
-    @add-edit-tunnel="reloadTunnels"
-    :is-shown="showCreateEditDrawer"
-  />
+  <DeleteTunnelModal :visible="showDeleteModal" :itemToDelete="selectedTunnelName" @close="showDeleteModal = false"
+    @tunnel-deleted="fetchTunnels" />
+  <GreDrawer :item-to-edit="selectedTunnel" @close="closeModalsAndDrawers" :rule-type="'forward'" :known-tags="[]"
+    @add-edit-tunnel="reloadTunnels" :is-shown="showCreateEditDrawer" />
+  <GreEdit :item-to-edit="selectedTunnels" @close="showEditModals = false" :rule-type="'forward'" :known-tags="[]"
+    @add-edit-tunnel="reloadTunnels" :is-shown="showEditModals" />
 </template>
-
-
